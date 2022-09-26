3c1d589c446c152e50a3d800206ebdfae3f47cef
maaeyeng <maaeyeng@MacBook-Pro-khxng-maaeyeng-2.local>
2022-09-26 18:10:29 +0700

    add

---
+++ document-app/resources/views/documents/index.blade.php
@@ -0,0 +1,11 @@
+@extends('documents.app')
+@section('content')
+<h1>Docuemnt Mangement System</h1>
+<ul>
+    <li>Create Documents</li>
+    <li>Create Rols</li>
+    <li>Add On</li>
+    <li>Send AS</li>
+    <li>Remove Product</li>
+</ul>
+@endsection
